56b9dbeff904727435096c3aed51de4e67331a9c
korycfitz <korycfitz@gmail.com>
2022-10-07 14:49:08 -0400

    finished excution

--- execution.rb
+++ execution.rb
@@ -43,9 +43,20 @@ list = [2, 3, -6, 7, -6, 7]
 p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 def largest_contiguous_subsum2(arr)
-    current_sum = 0
-    largest_sum = 0
-    arr.each do |ele|
+    current_sum = arr.first
+    largest_sum_so_far = arr.first
 
+    arr[1..-1].each do |ele|
+        current_sum += ele
+        current_sum = 0 if current_sum < 0 
+        if current_sum > largest_sum_so_far
+            largest_sum_so_far = current_sum
+        end
     end
+    return largest_sum_so_far
 end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum2(list) # => 8
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum2(list) # => 8 (from [7, -6, 7])
